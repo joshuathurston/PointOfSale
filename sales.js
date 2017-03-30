89b6db2539430d3449012551764dfa4aa035cea9
joshuathurston <jthurston3@mail.csuchico.edu>
2017-03-29 21:58:33 -0700

    cleaned and done with part1 x19

--- sales.js
+++ sales.js
@@ -17,16 +17,6 @@ function addItem()
     document.getElementById("price").value = "";
     setCookie("preTax", runningTotal, 1);
   }
-  //IF newItem is not a number
-  // THEN show an alert: "Enter price as a number"
-  //OTHERWISE,
-  // update newItem to its value cast as a number
-  // update runningTotal to be its value plus newItem
-  // create a variable called dollars
-  // call asCurrency() by with the value of runningTotal and assign the return value to dollars
-  // update the innerHTML of the span with the id "subtotal" to be dollars
-  // update the value of the input with the id "price" to be an empty string
-  // update a cookie called "preTax" with the value of runningTotal
 }
 
 //takes a number and gives a string with the number displayed as USD currency
